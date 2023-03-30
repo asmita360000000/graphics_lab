07dfd401a12b74e1264487b54f0be59b3f5ced35
Asmita <102935915+asmita360000000@users.noreply.github.com>
2023-03-30 06:56:45 +0545

    DDa

--- main.cpp
+++ main.cpp
@@ -1,43 +1,31 @@
 #include<graphics.h>
-#include<iostream>
-void drawline (int x0, int y0, int x1, int y1)
+#include<conio.h>
+#include<stdio.h>
+
+int main()
 {
-    int dx, dy,p, x,y;
-    dx= x1-x0;
-    dy= y1-y0;
+int gd = DETECT,gm,i;
+float x,y,dx,dy,steps;
+int x0,x1,y0,y1,m;
+initgraph(&gd, &gm,"");
+x0 = 50, y0 = 100, x1 = 250, y1 = 150;
+dx = (float)(x1-x0);
+dy = (float)(y1-y0);
+if(dx>=dy){
+    steps = dy;
 
-    x=x0;
-    y=y0;
-
-
-    p= 2*dy-dx;
-    while(x<x1);
-    {
-        if (p>=0)
-            {
-                putpixel(x,y,7);
-                y= y+1;
-                p=p+2*dy-2*dx;
-        }
-        else{
-            putpixel(x,y,7);
-            p=p+2*dy;
-        }
-        x= x+1;
+ }
+dx= dx/steps;
+dy= dy/steps;
+x = x0;
+y = y0;
+i=1;
+while(i<= steps){
+       putpixel(x,y,RED);
+       x += dx;
+       y += dy;
+       i = i+1;
     }
-
-}
-int main(){
-
-    int gdriver= DETECT, gmode, error, x0, y0, x1,y1;
-    initgraph(&gdriver, &gmode,"");
-    std:: cout<<"Enter co-ordinates of first point:";
-    std:: cin>>x0>>y0;
-    std::cout<<"Enter co-ordinates of second point:";
-    std:: cin>>x1>>y1;
-    drawline(x0, y0,x1,y1);
-
-
-
-        return 0;
+       getch();
+       closegraph();
 }
